8999b72b98bc454c64a136160a56db9fee8cb595
Felipe <116230329+FelipeLafuente@users.noreply.github.com>
2022-10-27 00:35:15 +0000

    Aula 1 - Sobre comentários/print

---
+++ Aula1.py
@@ -0,0 +1,35 @@
+# AULA 1 - 19/10/2022
+
+# Meu primeiro projeto Python!!! 
+
+# print() = comando de saida
+
+print("Hello World!")
+
+# Quando quiser guardar uma String! (format)
+
+nome = "Felipe Luiz Lafuente"
+
+# Quando quiser guardar um número inteiro
+
+idade = 19
+
+# Exibir o meu nome (que esta dentro da variável nome)
+print(nome,idade)
+
+# Quando quiser exibir a frase "Minha idade é" completando com o conteúdo variável idade
+
+print("Meu Nome é "+nome)
+print("Minha idade é "+str(idade)+" anos")
+print(f"Minha idade é {idade} anos")
+print("Minha idade é {} anos".format(idade))
+
+# Quando quiser exibir "Meu nome é... e tenho... anos " trocando pelas variavéis nome e idade
+
+print(f"Meu nome é {nome} e tenho {idade} anos")
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
+
+''' 
+Comentario em bloco
+
+'''
---
+++ Aula2A.py
@@ -0,0 +1,37 @@
+# AULA 2 - 26/10/2022
+
+# comando input(): quero permitir que o usuário digite algo 
+
+nome = input("Digite seu nome: ")
+
+# comando de saída - exibir na tela
+
+print(f"Boa noite, senhor(a) {nome}")
+
+# Pede a idade ao usuário - Qual sua idade?
+
+idade = int(input("Qual sua idade: "))
+
+# Exiba sua idade
+
+print(f"Sua idade é {idade}")
+
+# E seu eu quisesse mostra o dobro da idade informada
+
+dobro = idade * 2
+print(f"O dobro da idade informada é {dobro}")
+
+# Estrutura condicional - o famoso "SE" (if) 
+# Se a pessoa for maior de idade, mostre "Você é maior de idade, ótimo! Ja pode beber ou dirigir"
+
+if idade >= 18:
+  print("Você é maior de idade, ótimo! Já pode começar a beber ou dirigir")
+else:
+  print("Você é menor de idade, sem bebida para você")
+
+# E se eu quiser perguntar o genêro ( M = Masculino e F = Feminino)
+# Mostre se você precisa prestar ou prestou o servico militar obrigatório
+
+genero = input("Informe o genêro M = Masculino, F = Feminino, O = Outros: ")
+if idade >=18 and genero == "M":
+ print("... e você também precisa/precisou prestar serviço militar obrigatório")
