620b4cee7885be5d0de00aaaede1873e3f9306ce
Felipe <116230329+FelipeLafuente@users.noreply.github.com>
2022-11-17 00:37:47 +0000

    Aula 4 - Exemplo C

---
+++ Aula4C.py
@@ -0,0 +1,16 @@
+# Parte C da Aula 4 - 16/11/2022
+
+#1 Passo: importar a biblioteca sqlite3
+import sqlite3
+
+
+# PASSOS 2 e 3 VIRARÃO função conectar()
+def conectar():
+  #2o. passo: Vamos estabelecer uma
+  #conexão com o banco de dados
+  conexao = sqlite3.connect("dc_universe.db")
+
+  #3o. passo: criar um objeto do tipo cursor
+  cursor = conexao.cursor()
+
+  return conexao, cursor
